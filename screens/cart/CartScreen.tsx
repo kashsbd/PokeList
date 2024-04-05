989395e1a860ced3b786d9083f4ab1a7514aa360
kash <parakash.sbd@gmail.com>
2024-04-05 13:34:25 -0500

    feat: added home, detail and cart screen

---
+++ screens/cart/CartScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const CartScreen = () => {
+  return <View />;
+};
+
+export default CartScreen;
---
+++ screens/home/HomeScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {VStack} from '@gluestack-ui/themed';
+import {MasonryFlashList} from '@shopify/flash-list';
+
+import PokeListItem from './components/PokeListItem';
+import useInfiniteGetPokemonList from '../../hooks/useInfiniteGetPokemonList';
+
+import type {ListItemType} from '../../types/home';
+
+const HomeScreen = () => {
+  const {isPending, data, fetchNextPage, isFetchingNextPage, hasNextPage} =
+    useInfiniteGetPokemonList();
+
+  let allData = [];
+
+  if (!isPending && Array.isArray(data?.pages) && data.pages.length > 0) {
+    allData = data.pages.reduce(
+      (accumulator, currentValue) => [...accumulator, ...currentValue?.results],
+      [],
+    );
+  }
+
+  const renderItem = ({item}: {item: ListItemType}) => {
+    return <PokeListItem item={item} />;
+  };
+
+  return (
+    <VStack height="$full">
+      <MasonryFlashList
+        data={allData}
+        numColumns={2}
+        renderItem={renderItem}
+        estimatedItemSize={300}
+      />
+    </VStack>
+  );
+};
+
+export default HomeScreen;
