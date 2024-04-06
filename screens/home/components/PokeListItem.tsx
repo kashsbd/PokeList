39f0dcb12811997009d30d21da49b8183b40ceb1
kash <parakash.sbd@gmail.com>
2024-04-06 12:52:56 -0500

    feat: can navigate to detail screen

--- screens/home/components/PokeListItem.tsx
+++ screens/home/components/PokeListItem.tsx
@@ -1,34 +1,47 @@
 import React from 'react';
-import {VStack, Image, Text, HStack, Card, View} from '@gluestack-ui/themed';
+import {
+  VStack,
+  Image,
+  Text,
+  HStack,
+  Card,
+  View,
+  Pressable,
+} from '@gluestack-ui/themed';
+import {useNavigation} from '@react-navigation/native';
 
 import ToggleAddToCartButton from './ToggleAddToCartButton';
 
 import type {ListItemType} from '../../../types/home';
 
 const PokeListItem = ({item}: {item: ListItemType}) => {
+  const {navigate} = useNavigation();
+
   const onToggleAddToCartBtnPressed = (hasAdded: boolean) => {};
 
   return (
-    <Card m="$1">
-      <VStack>
-        <HStack justifyContent="center">
-          <Image
-            alt="image alt"
-            size="md"
-            source={{
-              uri: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-            }}
-          />
-        </HStack>
-        <Text textAlign="center">{item.name}</Text>
-        <View mt="$3">
-          <ToggleAddToCartButton
-            hasAlreadyAdded={false}
-            onPress={onToggleAddToCartBtnPressed}
-          />
-        </View>
-      </VStack>
-    </Card>
+    <Pressable onPress={() => navigate('Detail')}>
+      <Card m="$1">
+        <VStack>
+          <HStack justifyContent="center">
+            <Image
+              alt="image alt"
+              size="md"
+              source={{
+                uri: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+              }}
+            />
+          </HStack>
+          <Text textAlign="center">{item.name}</Text>
+          <View mt="$3">
+            <ToggleAddToCartButton
+              hasAlreadyAdded={false}
+              onPress={onToggleAddToCartBtnPressed}
+            />
+          </View>
+        </VStack>
+      </Card>
+    </Pressable>
   );
 };
 
